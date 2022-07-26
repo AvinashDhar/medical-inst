024929103d988c2e147f9e3b1f33a55e37484274
avinashdhar <avinashkumardhar@gmail.com>
2022-07-26 11:59:51 +0530

    added getUserAppointments route implementation

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,5 +1,29 @@
 const Appointment = require('../models/appointmentModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 exports.createAppointment = factory.createOne(Appointment);
-
 exports.getAppointments = factory.getAll(Appointment);
+exports.getUserAppointments =   catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (hack)
+    let filter = {};
+    if (req.params.userID) filter = { user: req.params.userID };
+    console.log(req.params.userID);
+    
+    const features = new APIFeatures(Appointment.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    // const doc = await features.query.explain();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc
+      }
+    });
+  })
--- routes/userAppointmentRoutes.js
+++ routes/userAppointmentRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.use(authController.protect);
 
 router.get('/',appointmentController.getAppointments);
+router.get('/user/:userID',appointmentController.getUserAppointments)
 router.post('/',appointmentController.createAppointment);
 
 module.exports = router;
